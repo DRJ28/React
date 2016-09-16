6a1ebacbfa0a884c860c9bc28df94ce6e08b17f9
DRJ28 <dheerajmehta10275@gmail.com>
2016-09-16 21:59:57 +0530

    basics of  react

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// set port 
+app.set('port', (process.env.PORT || 3000));
+// set static path
+app.use(express.static(path.join(__dirname)));
+// bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//start server
+app.listen(app.get('port'), function(){
+    console.log('Server has started at Port: '+ app.get('port'));
+});
